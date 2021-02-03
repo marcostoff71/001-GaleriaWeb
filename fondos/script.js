45b1b0dbc52bf9b89d63b746b340cbc3a76f3900
marco <marcostoff71@gmail.com>
2021-02-02 22:38:32 -0600

    Primera version

---
+++ fondos/script.js
@@ -0,0 +1,11 @@
+let nombre = document.getElementById("nombre2")
+let nom = prompt("introduce tu nombre")
+let apellido = prompt("introduse tu apellido")
+
+nombre.textContent = `Hola ${nom} ${apellido} Bienvenido`
+
+
+
+
+
+
---
+++ login/script.js
@@ -0,0 +1,11 @@
+const ingresar = document.getElementById('ingresar')
+const boton = document.getElementById('clic')
+
+ingresar.addEventListener('click',()=>{
+    console.log("hola");
+})
+
+boton.addEventListener('click',function(){
+console.log("jlla"),false
+})
+console.log('ingresar')
